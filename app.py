87cbe384e5ae13b533039e6b82f89195a17328d3
parthk5 <parth.khanna15@gmail.com>
2024-03-08 02:34:34 -0800

    Added command codes

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template, request, jsonify, session, abort, redi
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 import logging
-import uuid
 import generation
 import threading
 import time
+import os
 
 tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-small")
 model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-small")
@@ -13,7 +13,7 @@ logging.getLogger('transformers').setLevel(logging.ERROR)
 
 app = Flask(__name__)
 
-MAX_lIMIT_TEXT = 50
+MAX_LIMIT_TEXT = 50
 MAX_QUERY_LIMIT = 3
 
 
@@ -21,9 +21,19 @@ queryMap = {}
 connMap = {}
 #is_online = True
 
+def commence_shutdown():
+    time.sleep(5)
+    os._exit(0)
+
 
 @app.route("/")
 def home():
+    global MAX_LIMIT_TEXT
+    global MAX_QUERY_LIMIT
+
+    MAX_LIMIT_TEXT = MAX_LIMIT_TEXT
+    MAX_QUERY_LIMIT = MAX_QUERY_LIMIT
+
     client = f"{request.remote_addr}"
     #client = f"{request.remote_addr}:{request.environ.get('REMOTE_PORT')}"
 
@@ -37,6 +47,12 @@ def home():
 @app.route("/get", methods=['GET', 'POST'])
 def chat():
 
+    global MAX_LIMIT_TEXT
+    global MAX_QUERY_LIMIT
+
+    MAX_LIMIT_TEXT = MAX_LIMIT_TEXT
+    MAX_QUERY_LIMIT = MAX_QUERY_LIMIT
+
     client = f"{request.remote_addr}"
     #client = f"{request.remote_addr}:{request.environ.get('REMOTE_PORT')}"
 
@@ -46,16 +62,42 @@ def chat():
 
     print(f"Message Content Size: {len(message)} | Message Content: '{message}'")
     
-    if len(message) > MAX_lIMIT_TEXT:
+    if len(message) > MAX_LIMIT_TEXT:
         print("**** Detected chatbot tampering. Reload demanded ****")
         return "The chatbot has been tampered with. Reload the page for proper functionality"
     if len(message) == 0:
         print("**** Message with no content received ****")
         return "Please type in a valid prompt"
-    if message == "parthk2004xCLEAR":
-        queryMap.clear()
-        return "Override accepted. QueryMap has been cleared. Reload the page"
 
+    keycodes = open("../PortfolioChatBot Resources/OpenAI/overrides.txt", 'r').readlines()
+
+    if keycodes[-1][-1] != "\n":
+        keycodes[-1] = keycodes[-1]+"\n"
+
+    for keycode in keycodes:
+        if message == keycode[:-1]:
+            if keycodes.index(keycode) == 0:
+                queryMap.clear()
+                print(f"***** SYSTEM ADMIN: COMMAND CODE [{message}] ACCEPTED *****")
+                return "Override accepted. QueryMap has been cleared. Reload the page."
+            if keycodes.index(keycode) == 1:
+                queryMap.clear()
+                MAX_QUERY_LIMIT = 999
+                print(f"***** SYSTEM ADMIN: COMMAND CODE [{message}] ACCEPTED *****")
+                return "Override accepted. QueryLimit set to unlimited. Reload the page"
+            if keycodes.index(keycode) == 2:
+                queryMap.clear()
+                MAX_QUERY_LIMIT = 3
+                print(f"***** SYSTEM ADMIN: COMMAND CODE [{message}] ACCEPTED *****")
+                return "Override accepted. QueryLimit reset to 3. Reload the page"
+            if keycodes.index(keycode) == 3:
+                shutdown = threading.Thread(target=commence_shutdown, name="Shutdown ChatBot")
+                shutdown.start()
+                shutdown.join()
+                print(f"***** SYSTEM ADMIN: COMMAND CODE [{message}] ACCEPTED *****")
+                return "Override accepted. ChatBot terminated."
+        else:
+            print("Keycode matches failed")
 
     prompt = message
 
@@ -64,7 +106,7 @@ def chat():
             return "It's getting late, I'm going to go sleep 😴"
 
     try:
-        response = generation.gpt_gen(prompt, MAX_lIMIT_TEXT)
+        response = generation.gpt_gen(prompt, MAX_LIMIT_TEXT)
 
         queryMap[client] += 1
         print(f"**** {str(client)} has used up {queryMap[str(client)]}/{MAX_QUERY_LIMIT} queries ****")
