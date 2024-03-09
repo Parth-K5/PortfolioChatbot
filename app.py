01c974525e2dc7b045eceb47f6b1c9f7c788a085
parthk5 <parth.khanna15@gmail.com>
2024-03-08 22:31:35 -0800

    Swapped to session-based data storage such as queries
    Used flask's sessions for query constraints

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import generation
 import threading
 import time
 import os
+from uuid import uuid4
+
 
 tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-small")
 model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-small")
@@ -16,10 +18,10 @@ app.secret_key = "parthk5101x"
 
 MAX_LIMIT_TEXT = 50
 MAX_QUERY_LIMIT = 3
+RECENT_UID = ""
 
 
 queryMap = {}
-connMap = {}
 #is_online = True
 
 def commence_shutdown():
@@ -31,18 +33,38 @@ def commence_shutdown():
 def home():
     global MAX_LIMIT_TEXT
     global MAX_QUERY_LIMIT
+    global queryMap
 
     MAX_LIMIT_TEXT = MAX_LIMIT_TEXT
     MAX_QUERY_LIMIT = MAX_QUERY_LIMIT
+    queryMap = queryMap
 
     client = f"{request.remote_addr}"
     #client = f"{request.remote_addr}:{request.environ.get('REMOTE_PORT')}"
 
     print(f"Connected: {client}")
+
     #is_online = True
 
-    if str(client) not in queryMap:
-        queryMap[str(client)] = 0
+    identifier = uuid4()
+
+    if 'cached-query-count' not in session or len(queryMap) == 0:
+        session['cached-query-count'] = {}
+        session['cached-query-count'][str(identifier)] = 0
+        queryMap[str(identifier)] = 0
+        print(f"Registered {identifier} to {session['cached-query-count'][str(identifier)]}")
+    
+    if 'chatHistory' not in session:
+        session['chatHistory'] = {}
+        session['chatHistory']['lastRequest'] = ''
+        session['chatHistory']['lastReply'] = ''
+        print("Initialized chat history")
+
+    #print(queryMap)
+
+    
+    #if str(client) not in queryMap:
+        #queryMap[str(client)] = 0
     return render_template('index.html')
 
 @app.route("/get", methods=['GET', 'POST'])
@@ -50,18 +72,20 @@ def chat():
 
     global MAX_LIMIT_TEXT
     global MAX_QUERY_LIMIT
+    global queryMap
 
     MAX_LIMIT_TEXT = MAX_LIMIT_TEXT
     MAX_QUERY_LIMIT = MAX_QUERY_LIMIT
+    queryMap = queryMap
 
-
+    identifier = list(session['cached-query-count'].keys())[0]
+    #print(f"Found cached identifier: {identifier}")
 
     client = f"{request.remote_addr}"
     #client = f"{request.remote_addr}:{request.environ.get('REMOTE_PORT')}"
 
     message = request.form["msg"]
 
-    #print(f"QueryMap: {queryMap}")
     print("\n\n")
     print(f"Message Content Size: {len(message)} | Message Content: '{message}'")
     
@@ -80,46 +104,62 @@ def chat():
     for keycode in keycodes:
         if message == keycode[:-1]:
             if keycodes.index(keycode) == 0:
-                queryMap.clear()
+                queryMap[str(identifier)] = 0
+                session.clear()
+                session.modified = True
                 print(f"***** SYSTEM ADMIN: COMMAND CODE [{message}] ACCEPTED *****")
-                return "Override accepted. QueryMap has been cleared. Reload the page."
+                return f"Override accepted. QueryMap for user {identifier}@{client} has been cleared. Reload the page."
             if keycodes.index(keycode) == 1:
                 queryMap.clear()
+                session.clear()
+                session.modified = True
+                print(f"***** SYSTEM ADMIN: COMMAND CODE [{message}] ACCEPTED *****")
+                return f"Override accepted. System variable QueryMap has been cleared. Reload the page."
+            if keycodes.index(keycode) == 2:
+                queryMap[str(identifier)] = 0
+                session.clear()
+                session.modified = True
                 MAX_QUERY_LIMIT = 999
                 print(f"***** SYSTEM ADMIN: COMMAND CODE [{message}] ACCEPTED *****")
                 return "Override accepted. QueryLimit set to unlimited. Reload the page"
-            if keycodes.index(keycode) == 2:
-                queryMap.clear()
+            if keycodes.index(keycode) == 3:
+                queryMap[str(identifier)] = 0
                 MAX_QUERY_LIMIT = 3
+                session.clear()
+                session.modified = True
                 print(f"***** SYSTEM ADMIN: COMMAND CODE [{message}] ACCEPTED *****")
                 return "Override accepted. QueryLimit reset to 3. Reload the page"
-            if keycodes.index(keycode) == 3:
+            if keycodes.index(keycode) == 4:
+                print(f"***** SYSTEM ADMIN: COMMAND CODE [{message}] ACCEPTED *****")
                 shutdown = threading.Thread(target=commence_shutdown, name="Shutdown ChatBot")
                 shutdown.start()
                 shutdown.join()
-                print(f"***** SYSTEM ADMIN: COMMAND CODE [{message}] ACCEPTED *****")
                 return "Override accepted. ChatBot terminated."
 
     prompt = message
 
-    if 'requests' not in session:
-        print(f"Initialized cache for {client}")
-
-    session['requests'] = queryMap[str(client)]
-
-    print(f"{session['requests']} requests during session for {client}")
-
-    if session['requests'] >= MAX_QUERY_LIMIT:
+    if session['cached-query-count'][str(identifier)] >= MAX_QUERY_LIMIT:
             print("Session request limit hit")
             print("GPT--> ****[Sleep response posted]****")
             return "It's getting late, I'm going to go sleep 😴"
 
+    if prompt == session['chatHistory']['lastRequest']:
+        print("Duplicate request handled")
+        return session['chatHistory']['lastReply']
+
     try:
         response = generation.gpt_gen(prompt, MAX_LIMIT_TEXT)
 
-        queryMap[client] += 1
-        print(f"**** {str(client)} has used up {queryMap[str(client)]}/{MAX_QUERY_LIMIT} queries ****")
-    
+        session['cached-query-count'][str(identifier)] += 1
+        session['chatHistory']['lastRequest'] = prompt
+        session['chatHistory']['lastReply'] = response
+
+        print(f"{session['cached-query-count'][str(identifier)]} requests during session for {client}")
+        queryMap[str(identifier)] += 1
+        print(f"**** {str(client)} with UID: {identifier} has used up {queryMap[str(identifier)]}/{MAX_QUERY_LIMIT} queries ****")
+
+        session.modified = True
+
         return response
     except:
         print("**** Hit OpenAI Rate Limit ****")
